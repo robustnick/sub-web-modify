01972b4eea42d95a99840f6f268ccaaa5aa9f31c
robustnick <lq578653235@gmail.com>
2026-02-24 19:40:19 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
+          "CM负载均衡后端【vless reality+hy1+hy2】": "https://sub.zdhkcnls.qzz.io",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://api.v1.mk",
@@ -1304,3 +1304,4 @@ export default {
   }
 };
 </script>
+
